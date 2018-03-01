4fea2b5647cc1ae41f816bc3e2054923f24cbcb2
jvikraman <jvikraman@outlook.com>
2018-02-28 23:02:35 -0600

    initial commit

---
+++ js/example2.js
@@ -0,0 +1,37 @@
+class Score extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {score: 0};
+        this.incrementScore = this.incrementScore.bind(this);
+        this.decrementScore = this.decrementScore.bind(this);
+    }
+
+    incrementScore() {
+        this.setState({
+            score: this.state.score + 1
+        });
+    }
+
+    decrementScore() {
+        this.setState({
+            score: this.state.score - 1
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Score board for {this.props.teamName}</h2>
+                <div>
+                    Score: {this.state.score}<br/>
+                    <button onClick={this.incrementScore}>+</button>
+                    <button onClick={this.decrementScore}>-</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+var el = document.getElementById('root');
+ReactDOM.render(<Score teamName={"Tigers"}/>, el);
